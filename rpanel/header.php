bea3903f3c883da7bb8500266ae90d12d6c82d6d
Arijit Kundu <34868050+arikundu9@users.noreply.github.com>
2019-06-27 22:33:43 +0530

    assets move

--- rpanel/header.php
+++ rpanel/header.php
@@ -7,21 +7,21 @@
     <meta name="author" content="">
 	<meta name="theme-color" content="#609a41"/>
 
-    <link rel="icon" href="./assets/icons/favicon.ico">
+    <link rel="icon" href="../assets/icons/favicon.ico">
 
     <title><?=isset($title)?$title:"KBazar"?></title>
 
-    <link href="./assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="./assets/open-iconic/font/css/open-iconic-bootstrap.css" rel="stylesheet">
-    <link href="./assets/css/glyphicon.css" rel="stylesheet">
-    <link href="./assets/css/style.css?t=<?=filemtime('./assets/css/style.css')?>" rel="stylesheet">
-    <link href="./assets/css/sori.css?t=<?=filemtime('./assets/css/sori.css')?>" rel="stylesheet">
+    <link href="../assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../assets/open-iconic/font/css/open-iconic-bootstrap.css" rel="stylesheet">
+    <link href="../assets/css/glyphicon.css" rel="stylesheet">
+    <link href="../assets/css/style.css?t=<?=filemtime('../assets/css/style.css')?>" rel="stylesheet">
+    <link href="../assets/css/sori.css?t=<?=filemtime('../assets/css/sori.css')?>" rel="stylesheet">
 
-    <script src="./assets/jquery/jquery-3.3.1.min.js"></script>
+    <script src="../assets/jquery/jquery-3.3.1.min.js"></script>
 
-    <script src="./assets/js/popper.min.js"></script>
-    <script src="./assets/bootstrap/js/bootstrap.min.js"></script>
-    <script src="./assets/js/sori.js"></script>
-    <script src="./assets/js/script.js?t=<?=filemtime('./assets/js/script.js')?>"></script>
+    <script src="../assets/js/popper.min.js"></script>
+    <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="../assets/js/sori.js"></script>
+    <script src="../assets/js/script.js?t=<?=filemtime('../assets/js/script.js')?>"></script>
 </head>
 
