65f68dd1d03040efc8781348627e6a7e598dfb15
Arijit Kundu <34868050+arikundu9@users.noreply.github.com>
2019-06-28 23:26:31 +0530

    outer setup

---
+++ header.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+	<meta name="theme-color" content="#609a41"/>
+
+    <link rel="icon" href="./assets/icons/favicon.ico">
+
+    <title><?=isset($title)?$title:"KBazar"?></title>
+
+    <link href="./assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="./assets/open-iconic/font/css/open-iconic-bootstrap.css" rel="stylesheet">
+    <link href="./assets/css/glyphicon.css" rel="stylesheet">
+    <link href="./assets/css/kbstyle.css?t=<?=filemtime('./assets/css/kbstyle.css')?>" rel="stylesheet">
+    <link href="./assets/css/sori.css?t=<?=filemtime('./assets/css/sori.css')?>" rel="stylesheet">
+
+    <script src="./assets/jquery/jquery-3.3.1.min.js"></script>
+
+    <script src="./assets/js/popper.min.js"></script>
+    <script src="./assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="./assets/js/sori.js"></script>
+    <script src="./assets/js/script.js?t=<?=filemtime('./assets/js/script.js')?>"></script>
+</head>
+
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$title="Welcome To KBazar.";
+include 'header.php';
+?>
+<body>
+	<?php	//include 'head.php';
+			include 'nav.php'; 
+			//include 'msg.php';
+	?>
+	
+</body>
+<?php include 'footer.php';?>
